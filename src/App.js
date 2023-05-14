3d28b9c23faf5db31d28e6dfed383c95bd98a57c
Nikhil Saxena <nikhilsaxena003@gmail.com>
2023-05-15 02:26:33 +0530

    Addiing new pages and adding routing to the react app for easy page switching

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
+import { Routes, Route } from 'react-router-dom';
+
+//lets import the other pages in the component folder
+import AllMeetupsPage from './pages/AllMeetups';
+import NewMeetupPage from './pages/NewMeetup';
+import FavoritePage from './pages/Favorite';
+
 function App() {
   return (
-    <div></div>
+    <div>
+      <Routes>
+        <Route path='/' element={<AllMeetupsPage />} />
+        <Route path='/new-meetup' element={<NewMeetupPage />} />
+        <Route path='/favorites' element={<FavoritePage />} />
+      </Routes>
+    </div>
   );
 }
 
---
+++ src/pages/Favorite.js
@@ -0,0 +1,5 @@
+function FavoritePage () {
+    return <div>Favorites Page</div>
+}
+
+export default FavoritePage;
